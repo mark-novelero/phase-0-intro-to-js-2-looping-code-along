9010d80030c2f032e373e94e61977239d0f621b4
mark novelero <mark.novelero@gmail.com>
2021-03-19 15:20:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+var arrayNames = ['Ada', 'Brendan', 'Ali']; 
+var eventName = 'surprise'; 
+
+function writeCards(arrayNames, eventName){
+  var finalArray = [];
+  
+  for(var i = 0; i < arrayNames.length; i++){
+    var iArray = arrayNames[i]; 
+    var sentence = `Thank you, ${iArray}, for the wonderful ${eventName} gift!`;
+    finalArray.push(sentence); 
+  }
+
+  return finalArray; 
+}
+
+
+
+
+function countDown(){
+    var num = 10;
+
+    while(num >= 0){
+        console.log(num--);
+    }
+}
